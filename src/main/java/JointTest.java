8e145a3c3f4221c5e61d4a37143ccfa045be89d9
Lianne Meah <aca10lfm@sheffield.ac.uk>
2013-06-17 12:08:54 +0200

    final changes to JointTest - main still needs to be removed

--- src/main/java/JointTest.java
+++ src/main/java/JointTest.java
@@ -22,7 +22,7 @@ public class JointTest {
         private static RobotPositionMap myGoalPositions;
         
         /*
-         * Main method for testing
+         * Main method for testing/example usage
          * Purge later
          */
         public static void main(String[] args) {
@@ -153,82 +153,82 @@ public class JointTest {
         // completion message
   		System.out.println("Test complete using joint number: "+this.jointIndex);
   		double defaultPosition;
-  		// There has GOT to be an easier way of doing this...
+  		// run switch on joint index to retrieve default position
   		switch (this.jointIndex) {
 			// set joint
-			case 100: defaultPosition = 0.5; //DONE
+			case 100: defaultPosition = 0.5;
 				break;
-			case 200: defaultPosition = 0.5; //DONE
+			case 200: defaultPosition = 0.5;
 				break;
-			case 201: defaultPosition = 0.5; //DONE
+			case 201: defaultPosition = 0.5;
 				break;
-			case 202: defaultPosition = 0.5; //DONE
+			case 202: defaultPosition = 0.5;
 				break;
-			case 300: defaultPosition = 0.542372881355932; //DONE
+			case 300: defaultPosition = 0.542372881355932;
 				break;
-			case 301: defaultPosition = 0.442105263157895; //DONE
+			case 301: defaultPosition = 0.442105263157895;
 				break;
-			case 310: defaultPosition = 0.67; //DONE
+			case 310: defaultPosition = 0.67;
 				break;
-			case 311: defaultPosition = 0.5; //DONE
+			case 311: defaultPosition = 0.5;
 				break;
-			case 312: defaultPosition = 0.5; //DONE
+			case 312: defaultPosition = 0.5;
 				break;				
-			case 320: defaultPosition = 0.5; //DONE
+			case 320: defaultPosition = 0.5;
 				break;
-			case 321: defaultPosition = 0.5; //DONE
+			case 321: defaultPosition = 0.5;
 				break;				
-			case 322: defaultPosition = 0.364464692482916; //DONE
+			case 322: defaultPosition = 0.364464692482916;
 				break;
-			case 400: defaultPosition = 0.185185185185185; //DONE
+			case 400: defaultPosition = 0.185185185185185;
 				break; 
-			case 401: defaultPosition = 0.055555555555556; //DONE
+			case 401: defaultPosition = 0.055555555555556;
 				break;
-			case 410: defaultPosition = 0.5; //DONE
+			case 410: defaultPosition = 0.5;
 				break;
-			case 411: defaultPosition = 0.111111111111111; //DONE
+			case 411: defaultPosition = 0.111111111111111;
 				break;
-			case 420: defaultPosition = 0.5; //DONE
+			case 420: defaultPosition = 0.5;
 				break;
-			case 421: defaultPosition = 0.5; //grasp
+			case 421: defaultPosition = 0.5;
 				break;
-			case 500: defaultPosition = 0.185185185185185; //DONE
+			case 500: defaultPosition = 0.185185185185185;
 				break; 
-			case 501: defaultPosition = 0.055555555555556; //DONE
+			case 501: defaultPosition = 0.055555555555556;
 				break;
-			case 510: defaultPosition = 0.5; //DONE
+			case 510: defaultPosition = 0.5;
 				break;
-			case 511: defaultPosition = 0.111111111111111; //DONE
+			case 511: defaultPosition = 0.111111111111111;
 				break;
-			case 520: defaultPosition = 0.5; //DONE
+			case 520: defaultPosition = 0.5; 
 				break;
-			case 521: defaultPosition = 0.5; //grasp
+			case 521: defaultPosition = 0.5; 
 				break;
-			case 600: defaultPosition = 0.2; //DONE
+			case 600: defaultPosition = 0.2; 
 				break;
-			case 601: defaultPosition = 0.181818181818182; //DONE
+			case 601: defaultPosition = 0.181818181818182; 
 				break;	
-			case 602: defaultPosition = 0.1; //DONE
+			case 602: defaultPosition = 0.1; 
 				break;
-			case 610: defaultPosition = 0.909090909090909; //DONE
+			case 610: defaultPosition = 0.909090909090909; 
 				break;
-			case 620: defaultPosition = 0.7; //DONE
+			case 620: defaultPosition = 0.7; 
 				break;
-			case 621: defaultPosition = 0.5; //DONE
+			case 621: defaultPosition = 0.5; 
 				break;
-			case 700: defaultPosition = 0.2; //DONE
+			case 700: defaultPosition = 0.2; 
 				break;
-			case 701: defaultPosition = 0.818181818181818; //DONE
+			case 701: defaultPosition = 0.818181818181818; 
 				break;	
-			case 702: defaultPosition = 0.1; //DONE
+			case 702: defaultPosition = 0.1;
 				break;
-			case 710: defaultPosition = 0.909090909090909; //DONE
+			case 710: defaultPosition = 0.909090909090909;
 				break;
-			case 720: defaultPosition = 0.7; //DONE
+			case 720: defaultPosition = 0.7; 
 				break;
-			case 721: defaultPosition = 0.5; //DONE
+			case 721: defaultPosition = 0.5; 
 				break;				
-			default: defaultPosition = 0.5; //DONE
+			default: defaultPosition = 0.5;
 		}
 		if (printOut) {
 			System.out.println("Now setting joint to default value: "+defaultPosition);
